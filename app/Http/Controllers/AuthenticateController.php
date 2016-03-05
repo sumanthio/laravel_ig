4db890eb69f13db18e5c43fb55e61b36d8b92c32
sumanthio <sumanthp544@gmail.com>
2016-03-05 22:59:29 +0530

    initial commit

---
+++ app/Http/Controllers/AuthenticateController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use JWTAuth;
+use Tymon\JWTAuth\Exception\JWTException;
+
+class AuthenticateController extends Controller
+{
+    public function index() {
+
+    }
+
+    public function authenticate (Request $request){
+      $credentials  = $request->only('email','password');
+      try{
+        //dd($credentials);
+        if(!$token = JWTAuth::attempt($credentials)){
+          return response()->json(['error'=>'invalid_credentials'],401);
+        }
+      }
+      catch (JWTException $e){
+        return response()->json(['error'=>'no token created'],500);
+      }
+
+      return response()->json(compact('token'));
+    }
+}
